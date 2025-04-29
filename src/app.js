281ca2090523bce2539e5e0337a5748ee73f982a
Justin Chow <justin.chow05@gmail.com>
2025-04-29 18:32:57 +0700

    feat: add ping endpoint

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/ping', (req, res) => {
-    res.status(200).json('pong');
+    return res.status(200).json({ message: 'pong' });
 });
 
 app.use('/tenant', tenantRoutes)
