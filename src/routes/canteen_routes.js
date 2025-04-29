391bb8559b3b157bb8a36a08d26c00a52679e56d
Raditya Yusuf <radityayusuf002@gmail.com>
2025-04-29 19:31:44 +0700

    fix: public can access get all canteens

--- src/routes/canteen_routes.js
+++ src/routes/canteen_routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('', authenticate, authorize("tenant"), createCanteen);
 
-router.get('/all', authenticate, authorize("tenant"), getAllCanteens);
+router.get('/all', getAllCanteens);
 
 module.exports = router;
